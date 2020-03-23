a1263ab789e456031f883da8397bdc5173d8e8a6
Ron Planken <ron.planken@pon.com>
2020-03-23 14:38:01 +0100

    Changed test to include pagenumber and removed min

--- paginator.js
+++ paginator.js
@@ -3,9 +3,10 @@ const paginator = (result) => {
   if (result.items.length > 0) {
     const totalPages = result.totalPages;
     const pageNumber = ++result.pageNumber;
+
     const firstItem = (result.pageNumber * result.itemsPerPage) + 1;
-    const lastItem = Math.min(firstItem + (result.itemsPerPage), result.totalElements);
-    return `${firstItem}-${lastItem} van ${result.totalElements} items`;
+    const lastItem = (firstItem + result.itemsPerPage);
+    return `Pagina: ${pageNumber} | ${firstItem}-${lastItem} van ${result.totalElements} items`;
   } else {
     return 'Geen items gevonden';
   }
